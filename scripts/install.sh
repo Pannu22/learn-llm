f355ab1ceef043b3a7dc95dda6775ae319ba43b7
Narpinder Singh <narpinder.singh.pannu@koireader.com>
2026-08-26 20:16:50 +0530

    chore: customer support rag
    Customer support RAG using owner's
    manuals.

--- scripts/install.sh
+++ scripts/install.sh
@@ -8,7 +8,7 @@ conda create --name $py_env python=3.13
 conda activate $py_env
 echo "Env activated $py_env"
 
-pip install -q 'litellm[google]' langchain langchain-community langchain-openai langchain-litellm redis redisvl python-dotenv
+pip install -q 'litellm[google]' langchain langchain-community langchain-openai langchain-litellm redis redisvl python-dotenv chromadb sentence-transformers pypdf pymupdf pandas rank_bm25
 
 echo "Setup done. Execute:-"
 echo "conda activate $py_env"
